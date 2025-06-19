2561c003e2db87d657df66d38a168011dcf6c548
sidvisjes <jan@knoops.org>
2025-06-19 08:40:59 +0200

    Update smtp_test.py

--- smtp_test.py
+++ smtp_test.py
@@ -1,22 +1,25 @@
 import smtplib
-from email.message import EmailMessage
 import os
+from email.message import EmailMessage
 
-# Zet deze eventueel hardcoded om te testen
-SMTP_USER ="postmaster@sandbox4310c577257a42ba9932bc55e335514a.mailgun.org"
-SMTP_PASSWORD = "1ce77a6efc563b1881ac29cb0c602be2-51afd2db-0e23a016"
-MAIL_TO = "jan@knoops.org"
+smtp_user = os.environ.get("SMTP_USER")
+smtp_password = os.environ.get("SMTP_PASSWORD")
+mail_to = os.environ.get("MAIL_TO")
+
+if not all([smtp_user, smtp_password, mail_to]):
+    raise Exception("SMTP_USER, SMTP_PASSWORD of MAIL_TO ontbreekt in de environment")
 
 msg = EmailMessage()
-msg["Subject"] = "✅ Testmail via Mailgun SMTP"
-msg["From"] = SMTP_USER
-msg["To"] = MAIL_TO
-msg.set_content("Dit is een testmail verstuurd via smtp.mailgun.org met poort 465 en SSL.")
+msg["Subject"] = "✅ Mailgun SMTP-test geslaagd"
+msg["From"] = smtp_user
+msg["To"] = mail_to
+msg.set_content("Dit is een testmail via Mailgun SMTP vanaf GitHub Actions.")
 
 try:
-    with smtplib.SMTP_SSL("smtp.mailgun.org", 465) as smtp:
-        smtp.login(SMTP_USER, SMTP_PASSWORD)
+    with smtplib.SMTP("smtp.mailgun.org", 587) as smtp:
+        smtp.starttls()
+        smtp.login(smtp_user, smtp_password)
         smtp.send_message(msg)
-        print("✅ Testmail verzonden!")
+        print("✅ Testmail succesvol verzonden!")
 except Exception as e:
     print("❌ Fout bij verzenden testmail:", e)
